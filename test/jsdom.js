9ea0a34574d6f0dfcb40b672b4f0a48d7aa2297b
Tom Kraak <thomas.kraak@teamaol.com>
2017-09-27 16:44:50 -0400

    fire event

--- test/jsdom.js
+++ test/jsdom.js
@@ -1,15 +1,26 @@
 import test from 'ava'
 import { JSDOM } from 'jsdom'
+import simulant from 'jsdom-simulant'
 
-const dom = new JSDOM(`<!DOCTYPE html><p>Hello world</p>`)
-const $ = require('jquery')(dom.window) // eslint-disable-line no-unused-vars
-const qs = dom.window.document.querySelector('p').textContent
-const jq = $('p').text()
+const dom = new JSDOM(`<!DOCTYPE html><p>Hello <a href="#">world</a></p>`)
+const window = dom.window
+const document = dom.window.document
+const $ = require('jquery')(window)
 
-test('querySelector', t => {
+test('native selection', t => {
+  const qs = document.querySelector('p').textContent
   t.is(qs, 'Hello world')
 })
 
-test('jquery', t => {
+test('jquery selection', t => {
+  const jq = $('p').text()
   t.is(jq, 'Hello world')
+})
+
+test('fire event', t => {
+  $('a').on('click', () => { $('p').text('Hello test') })
+  const event = simulant(window, 'click')
+  const element = document.querySelector('a')
+  simulant.fire(element, event)
+  t.is($('p').text(), 'Hello test')
 })

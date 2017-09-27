c2b4e8930ff4ea90df97b6e64572888fec622d90
Tom Kraak <thomas.kraak@teamaol.com>
2017-09-27 13:52:44 -0400

    add jQuery

--- test/jsdom.js
+++ test/jsdom.js
@@ -2,8 +2,14 @@ import test from 'ava'
 import { JSDOM } from 'jsdom'
 
 const dom = new JSDOM(`<!DOCTYPE html><p>Hello world</p>`)
-const textContent = dom.window.document.querySelector('p').textContent
+const $ = require('jquery')(dom.window) // eslint-disable-line no-unused-vars
+const qs = dom.window.document.querySelector('p').textContent
+const jq = $('p').text()
 
-test('test', t => {
-  t.is(textContent, 'Hello world')
+test('querySelector', t => {
+  t.is(qs, 'Hello world')
+})
+
+test('jquery', t => {
+  t.is(jq, 'Hello world')
 })
